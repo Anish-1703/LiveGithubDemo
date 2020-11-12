b662c81ce25d6a60e5091745fba1373756ce87d8
Anish-1703 <71100656+Anish-1703@users.noreply.github.com>
2020-11-12 14:01:31 -0800

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Following is the code where a Cat greets the Students of CS 151
+
+        System.out.println("   /\\_/\\     ------ ");
+        System.out.println("  / o o \\  / Welcome \\");
+        System.out.println(" (   \"   ) <   All To    |");
+        System.out.println("  \\~(*)~/  \\ GitHub! /");
+        System.out.println("   // \\\\     ------ ");
+
+    }
+}
